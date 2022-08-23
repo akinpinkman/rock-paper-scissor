876b731e0eba86b9175715c634419db9eb011d72
akinpinkman <akinbilgin54@hotmail.com>
2022-08-23 18:59:59 +0300

    Computer selecting Rock, Paper or Scissor

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+1. If both make the same move, declare the game a draw.
+2. If playerSelection 'Rock' and computerSelection 'Scissor' player wins.
+3. If playerSelection 'Scissor' and computerSelection 'Paper' player wins.
+4. If playerSelection 'Paper' and computerSelection 'Rock' player wins.
+5. Else computer wins.
+*/
+
+let choices = ['Rock', 'Paper', 'Scissor']
+let random = choices[Math.floor(Math.random() * choices.length)]
+
+
+
+
